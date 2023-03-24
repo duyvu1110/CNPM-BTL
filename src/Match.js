0cbd4fa9a7de7c9c5ae3daf93f4b2f11e08bc874
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2023-03-25 01:43:44 +0700

    .

--- src/Match.js
+++ src/Match.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Board from './Board'; 
+import Board from './Board';
 import './style.css';
 function Match() {
   const [matchData, setMatchData] = useState(null);
@@ -20,21 +20,36 @@ function Match() {
   return (
     <div>
       <p className='match_id'>Match_ID: {matchData.match_id}</p>
-      {matchData.status !== "None" &&
+      <div className='match-in4'>
+        <div className='team1-in4'>
+          <span className='team1_id'>{matchData.team1_id}</span>
+          <span className='time-left'>Time left: {matchData.time1}</span>
+        </div>
+        <div className='img1'></div>
+        <div className='scored'>
+          <span> {matchData.score1}-{matchData.score2}</span>
+        </div>
+        <div className='img2'></div>
+        <div className='team2-in4'>
+          <span className='team2_id'> {matchData.team2_id}</span>
+          {/* <div className='ava2'></div> */}
+          <span className='time-left'>Time left: {matchData.time2}</span>
+        </div>
+      </div>
+
+
+      <div className='turn'>
+        <span>{matchData.turn}</span>
+        <span>'s</span> 
+        <span>turn</span>
+        </div>
+      
+      <Board matchData={matchData} />
+      {/* {matchData.status !== "None" &&
         <p>Status: {matchData.status}</p>
-      }
-      
-      <p>Time left for team1: {matchData.time1}</p>
-      <p>Time left for team2: {matchData.time2}</p>
-      <p>Team1 ID: {matchData.team1_id}</p>
-      <p>Team2 ID: {matchData.team2_id}</p>
-      
-      <div>{matchData.turn}'s turn</div>
-      <p>Score: {matchData.score1}-{matchData.score2}</p>
-      <Board matchData={matchData}/>
+      } */}
     </div>
   );
 }
 
 export default Match;
-
